c5b845ca1334b080e80653367916c88e27024924
Brian Lusina <lusinabrian@gmail.com>
2017-04-24 21:58:42 +0300

    replace strings in roguereducer with constant action types

--- src/reducers/rogueLikeReducer.js
+++ src/reducers/rogueLikeReducer.js
@@ -1,5 +1,6 @@
 import initialState from './initialState';
-import * as types from '../constants/gameConstants';
+import * as gConstants from '../constants/gameConstants';
+import * as types from '../constants/actionTypes';
 import _ from 'lodash';
 
 /**
@@ -15,52 +16,53 @@ export default function rogueLikeReducer(state = initialState, action) {
     let _extends2, _extends3, _extends4, _extends5, _extends6, _extends7, _occupiedSpaces, _extends8;
 
     switch (action.type) {
-        case 'DAMAGE':
-            return types._extends({}, state, {
-                entities: types._extends({},
+        case types.DAMAGE:
+            return gConstants._extends({}, state, {
+                entities: gConstants._extends({},
                     state.entities,
-                    (_extends2 = {}, _extends2[action.entityName] = types._extends({}, state.entities[action.entityName], {
+                    (_extends2 = {}, _extends2[action.entityName] = gConstants._extends({}, state.entities[action.entityName], {
                         health: state.entities[action.entityName].health - action.value
                     }), _extends2)
                 )
             });
-        case 'HEAL':
-            return types._extends({}, state, {
-                entities:types. _extends({}, state.entities, (_extends3 = {}, _extends3[action.entityName] = types._extends({}, state.entities[action.entityName], {
+        case types.HEAL:
+            return gConstants._extends({}, state, {
+                entities: gConstants._extends({}, state.entities, (_extends3 = {}, _extends3[action.entityName] = gConstants._extends({}, state.entities[action.entityName], {
                     health: state.entities.player.health + action.value
                 }), _extends3))
             });
 
-        case 'SWITCH_WEAPON':
-            return types._extends({}, state, {
-                entities: types._extends({}, state.entities, {
-                    'player': types._extends({}, state.entities.player, {
+        case types.SWITCH_WEAPON:
+            return gConstants._extends({}, state, {
+                entities: gConstants._extends({}, state.entities, {
+                    'player': gConstants._extends({}, state.entities.player, {
                         weapon: action.weapon,
                         attack: state.entities.player.attack + action.attack
                     })
                 })
             });
 
-        case 'MOVE':
-            return types._extends({}, state, {
+        case types.MOVE:
+            return gConstants._extends({}, state, {
                 occupiedSpaces: _.chain(state.occupiedSpaces).omit(state.entities[action.entityName].x + 'x' + state.entities[action.entityName].y).set(state.entities[action.entityName].x + action.vector.x + 'x' + (state.entities[action.entityName].y + action.vector.y), action.entityName).value(),
-                entities: types._extends({}, state.entities, (_extends4 = {}, _extends4[action.entityName] = types._extends({}, state.entities[action.entityName], {
+                entities: gConstants._extends({}, state.entities, (_extends4 = {}, _extends4[action.entityName] = gConstants._extends({}, state.entities[action.entityName], {
                     x: state.entities[action.entityName].x + action.vector.x,
                     y: state.entities[action.entityName].y + action.vector.y
                 }), _extends4))
             });
-        case 'SET_LOCATION':
-            return types._extends({}, state, {
+
+        case types.SET_LOCATION:
+            return gConstants._extends({}, state, {
                 occupiedSpaces: _.chain(state.occupiedSpaces).omit(state.entities[action.entityName].x + 'x' + state.entities[action.entityName].y).set(action.location.x + 'x' + action.location.y, action.entityName).value(),
-                entities: types._extends({}, state.entities, (_extends5 = {}, _extends5[action.entityName] = types._extends({}, state.entities[action.entityName], {
+                entities: gConstants._extends({}, state.entities, (_extends5 = {}, _extends5[action.entityName] = gConstants._extends({}, state.entities[action.entityName], {
                     x: action.location.x,
                     y: action.location.y
                 }), _extends5))
             });
-        case 'ADD_ENTITY':
-            return types._extends({}, state, {
-                occupiedSpaces: types._extends({}, state.occupiedSpaces, (_extends6 = {}, _extends6[action.location.x + 'x' + action.location.y] = action.entityName, _extends6)),
-                entities: types._extends({}, state.entities, (_extends7 = {}, _extends7[action.entityName] = {
+        case types.ADD_ENTITY:
+            return gConstants._extends({}, state, {
+                occupiedSpaces: gConstants._extends({}, state.occupiedSpaces, (_extends6 = {}, _extends6[action.location.x + 'x' + action.location.y] = action.entityName, _extends6)),
+                entities: gConstants._extends({}, state.entities, (_extends7 = {}, _extends7[action.entityName] = {
                     entityType: action.entityType,
                     health: action.health,
                     attack: action.attack,
@@ -68,47 +70,47 @@ export default function rogueLikeReducer(state = initialState, action) {
                     y: action.location.y
                 }, _extends7))
             });
-        case 'REMOVE_ENTITY':
-            return types._extends({}, state, {
+        case types.REMOVE_ENTITY:
+            return gConstants._extends({}, state, {
                 occupiedSpaces: _.chain(state.occupiedSpaces).omit(state.entities[action.entityName].x + 'x' + state.entities[action.entityName].y).value(),
                 entities: _.chain(state.entities).omit(action.entityName).value()
             });
-        case 'RESET_BOARD':
-            return types._extends({}, state, {
+        case types.RESET_BOARD:
+            return gConstants._extends({}, state, {
                 entities: {
                     'player': state.entities.player
                 },
                 occupiedSpaces: (_occupiedSpaces = {}, _occupiedSpaces[state.entities.player.x + 'x' + state.entities.player.y] = 'player', _occupiedSpaces)
             });
-        case 'SET_MAP':
-            return types._extends({}, state, {
+        case types.SET_MAP:
+            return gConstants._extends({}, state, {
                 gameMap: action.map
             });
-        case 'INCREASE_LEVEL':
-            return types._extends({}, state, {
+        case types.INCREASE_LEVEL:
+            return gConstants._extends({}, state, {
                 level: state.level + 1
             });
-        case 'RESET_LEVEL':
-            return types._extends({}, state, {
+        case types.RESET_LEVEL:
+            return gConstants._extends({}, state, {
                 level: 0
             });
-        case 'SET_WINDOW_SIZE':
-            return types._extends({}, state, {
+        case types.SET_WINDOW_SIZE:
+            return gConstants._extends({}, state, {
                 windowHeight: action.windowHeight,
                 windowWidth: action.windowWidth
             });
-        case 'GAIN_XP':
-            return types._extends({}, state, {
-                entities: types._extends({}, state.entities, {
-                    'player': types._extends({}, state.entities.player, {
+        case types.GAIN_XP:
+            return gConstants._extends({}, state, {
+                entities: gConstants._extends({}, state.entities, {
+                    'player': gConstants._extends({}, state.entities.player, {
                         toNextLevel: state.entities.player.toNextLevel - action.xp
                     })
                 })
             });
-        case 'LEVEL_UP':
-            return types._extends({}, state, {
-                entities: types._extends({}, state.entities, {
-                    'player': types._extends({}, state.entities.player, {
+        case types.LEVEL_UP:
+            return gConstants._extends({}, state, {
+                entities: gConstants._extends({}, state.entities, {
+                    'player': gConstants._extends({}, state.entities.player, {
                         attack: state.entities.player.attack + action.attack,
                         health: state.entities.player.health + action.health,
                         toNextLevel: action.toNextLevel,
@@ -116,14 +118,15 @@ export default function rogueLikeReducer(state = initialState, action) {
                     })
                 })
             });
-        case 'RESET_MAP':
-            return types._extends({}, initialState, {
+        case types.RESET_MAP:
+            return gConstants._extends({}, initialState, {
                 gameMap: action.map
             });
-        case 'ADD_BOSS':
-            return types._extends({}, state, {
-                occupiedSpaces: types._extends({}, state.occupiedSpaces, (types._extends8 = {}, types._extends8[action.location.x + 'x' + action.location.y] = 'boss', types._extends8[action.location.x + 1 + 'x' + action.location.y] = 'boss', types._extends8[action.location.x + 'x' + (action.location.y + 1)] = 'boss', types._extends8[action.location.x + 1 + 'x' + (action.location.y + 1)] = 'boss', types._extends8)),
-                entities: types._extends({}, state.entities, {
+
+        case types.ADD_BOSS:
+            return gConstants._extends({}, state, {
+                occupiedSpaces: gConstants._extends({}, state.occupiedSpaces, (gConstants._extends8 = {}, gConstants._extends8[action.location.x + 'x' + action.location.y] = 'boss', gConstants._extends8[action.location.x + 1 + 'x' + action.location.y] = 'boss', gConstants._extends8[action.location.x + 'x' + (action.location.y + 1)] = 'boss', gConstants._extends8[action.location.x + 1 + 'x' + (action.location.y + 1)] = 'boss', gConstants._extends8)),
+                entities: gConstants._extends({}, state.entities, {
                     boss: {
                         entityType: 'enemy',
                         health: action.health,
@@ -133,8 +136,9 @@ export default function rogueLikeReducer(state = initialState, action) {
                     }
                 })
             });
-        case 'TOGGLE_DARKNESS':
-            return types._extends({}, state, {
+
+        case types.TOGGLE_DARKNESS:
+            return gConstants._extends({}, state, {
                 darkness: !state.darkness
             });
         default:
