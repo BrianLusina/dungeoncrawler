3e48e45efc07bb55c10c427db3c7f3f4a0636953
Brian Lusina <lusinabrian@gmail.com>
2017-05-09 22:05:09 +0300

    Configure store with middlware
    Middleware will add redux-thunk, composeWithDevTools to store
    Provider wraps the container component for the application

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
 import App from './containers/App';
+import configureStore from './store/configureStore';
 import './styles/sass/index.css';
+
+// initialize store
+const store = configureStore();
 
 // Entry point of the application. This will render the application to the DOM.
 render(
-    <App />,
+    <Provider store={store}>
+        <App />
+    </Provider>,
     document.getElementById('root')
 );
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,11 +1,13 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer  from '../reducers/rootReducer';
+import { enableBatching } from 'redux-batched-actions';
+import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 const configureStore = () => {
     return createStore(
         rootReducer,
-        composeWithDevTools()
+        applyMiddleware(thunk, composeWithDevTools)
     )
 };
 
