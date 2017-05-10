5393c4baf917d7fc4daba8879c4a7041ada8a69a
Brian Lusina <lusinabrian@gmail.com>
2017-05-10 22:13:23 +0300

    Test for tips container
    test to check if container renders
    has div element
    p tag with tips
    and css classes are on the correct elements

--- tests/Tips.test.js
+++ tests/Tips.test.js
@@ -8,3 +8,28 @@ import { shallow }from 'enzyme';
 import React from 'react';
 import Tips from '../src/containers/Tips';
 
+describe("Test Tips Container", function () {
+    var wrapper;
+
+    beforeEach(function () {
+        wrapper = shallow(<Tips/>);
+    });
+
+    it("should render as expected", function () {
+        const div = document.createElement("div");
+        render(<wrapper />, div);
+    });
+
+    it("should have 1 div element", ()=>{
+       expect(wrapper.find("div").length).toEqual(1)
+    });
+
+    it("div should have class of strip", function () {
+        expect(wrapper.find("div").hasClass("strip")).toBe(true);
+    });
+
+    it("div should have 1 p tag with a tip", function () {
+        expect(wrapper.find("p").length).toEqual(1);
+    });
+
+})
