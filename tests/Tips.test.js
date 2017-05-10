c614b36f0ecfb12d1098ab8cf10a15889764fb85
Brian Lusina <lusinabrian@gmail.com>
2017-05-10 22:07:35 +0300

    INstall enzyme
    usefull for running tests on the DOM

---
+++ tests/Tips.test.js
@@ -0,0 +1,10 @@
+/**
+ * @author lusinabrian on 10/05/17.
+ * @notes: Tests for Tips component
+ */
+import { render } from 'react-dom';
+import expect from 'expect';
+import { shallow }from 'enzyme';
+import React from 'react';
+import Tips from '../src/containers/Tips';
+
